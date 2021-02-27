7f0e6420734ae63fe184abb7aa54563849313365
aigulct <aigulct@gmail.com>
2021-02-26 22:11:51 -0800

    Added DetailActivity.

--- app/src/main/java/com/example/flixster/models/Movie.java
+++ app/src/main/java/com/example/flixster/models/Movie.java
@@ -1,21 +1,32 @@
 package com.example.flixster.models;
 
+import android.widget.RatingBar;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Parcel
 public class Movie {
+
+    int movieId;
     String posterPath;
     String title;
     String overview;
+    double ratingBar;
+
+    public Movie() {}
 
     public Movie(JSONObject jsonObject) throws JSONException {
         posterPath = jsonObject.getString("poster_path");
         title = jsonObject.getString("title");
         overview = jsonObject.getString("overview");
+        ratingBar = jsonObject.getDouble("vote_average");
+        movieId = jsonObject.getInt("id");
     }
 
     public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
@@ -36,5 +47,13 @@ public class Movie {
 
     public String getOverview() {
         return overview;
+    }
+
+    public double getRatingBar() {
+        return ratingBar;
+    }
+
+    public int getMovieId() {
+        return movieId;
     }
 }
